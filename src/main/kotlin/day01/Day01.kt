fa646cbd22c1afea9068a627e05d7ac6a32f3245
Sylvain Moindron <sylvain@moindron.net>
2024-12-01 12:23:15 +0100

    day01

---
+++ src/main/kotlin/day01/Day01.kt
@@ -0,0 +1,54 @@
+package org.example.day01
+
+import java.io.File
+
+class Day01 {
+
+    private val list01: List<Int>
+    private val list02: List<Int>
+
+    init {
+
+
+        val input =
+            File("/data/home/sylvain/IdeaProjects/advent2024/src/main/resources/day01.txt").bufferedReader()
+                .lineSequence()
+                .map { line ->
+                    val splited = line.split("   ")
+                    arrayOf(
+                        splited[0].toInt(), splited[1].toInt()
+                    )
+                }.toList()
+
+        list01 = input.map { it[0] }
+        list02 = input.map { it[1] }
+
+    }
+
+    fun part01() {
+        val list1Sorted = list01.sorted()
+        val list2Sorted = list02.sorted()
+
+        val sumOfDistance = list1Sorted.zip(list2Sorted).map { Math.abs(it.first - it.second) }.sum()
+        println(" sumOfDistance= $sumOfDistance")
+    }
+
+
+    fun part02() {
+        val countList02 = mutableMapOf<Int,Int>()
+        list02.asSequence().forEach {
+            countList02[it] = 1 + (countList02[it]?:0)
+        }
+
+       val similarityScore= list01.asSequence().map {
+            it*(countList02[it]?:0)
+        }.sum()
+        println ("similarityScore = $similarityScore")
+    }
+}
+
+fun main() {
+    val day = Day01()
+//    day.part01()
+    day.part02()
+}
