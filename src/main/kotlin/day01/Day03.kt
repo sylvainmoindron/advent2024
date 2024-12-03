5ae6bf244ccfa51c224d4b20e571596f395ca06a
sylvain moindron <sylvain.moindron@external.engie.com>
2024-12-03 10:33:48 +0100

    day 3

---
+++ src/main/kotlin/day01/Day03.kt
@@ -0,0 +1,45 @@
+package day01
+
+import java.io.File
+
+class Day03 : Day() {
+
+
+    val input = File("src/main/resources/day03.txt").readText()
+
+
+    override fun part01() {
+        val sum = Regex("mul\\((\\d+),(\\d+)\\)").findAll(input).map {
+            it.groupValues[1].toInt() * it.groupValues[2].toInt()
+        }.sum()
+        println("sum= $sum")
+    }
+
+    override fun part02() {
+        var enabled = true
+
+
+        val sum=Regex("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)").findAll(input).map {
+            if(it.value == "do()") {
+                enabled = true
+                    0
+            } else if(it.value == "don't()") {
+                enabled = false
+                0
+            } else if(enabled) {
+                it.groupValues[1].toInt() * it.groupValues[2].toInt()
+            } else {
+                0
+            }
+        }.sum()
+
+        println("sumEnabler= $sum")
+
+
+    }
+}
+
+fun main() {
+    val day = Day03()
+    day.solve()
+}
